8962dbd8d8718f719e8d6f20d29941306ddd97ba
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-02 16:23:57 +0300

    FAM: Base Icon created and placed

--- FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
+++ FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // in Activity Context
+        ImageView imageView1 = new ImageView(this); // Create an icon
+        imageView1.setImageResource(R.drawable.ic_launcher);
+
+        com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton actionButton =
+                new com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.Builder(this)
+                .setContentView(imageView1)
+                .build();
+
+
     }
 
     @Override
