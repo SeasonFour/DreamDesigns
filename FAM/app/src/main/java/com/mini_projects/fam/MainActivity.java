585f77855a00ee91ba9267869ca088a1a024e438
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-02 17:06:40 +0300

    MainActivity.java: Four items added to the FAM successfully

--- FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
+++ FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
@@ -1,16 +1,14 @@
 package com.mini_projects.fam;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // 1. Create a button to attach the menu:
         // in Activity Context
         ImageView imageView1 = new ImageView(this); // Create an icon
         imageView1.setImageResource(R.drawable.ic_launcher);
@@ -27,6 +27,41 @@ public class MainActivity extends AppCompatActivity {
                 new com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.Builder(this)
                 .setContentView(imageView1)
                 .build();
+
+        // 2. Creating Menu Items
+        // Icons for the 'appearing' objects - repeat many times:
+        ImageView itemIcon1 = new ImageView(this);
+        itemIcon1.setImageResource(R.drawable.arena_50);
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageResource(R.drawable.coins_50);
+
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageResource(R.drawable.phone_50);
+
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageResource(R.drawable.wifi_50);
+
+        //Creating Menu Items - buttons for the 4 icons
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+        SubActionButton buttonIcon1 = itemBuilder.setContentView(itemIcon1).build();
+        SubActionButton buttonIcon2 = itemBuilder.setContentView(itemIcon2).build();
+        SubActionButton buttonIcon3 = itemBuilder.setContentView(itemIcon3).build();
+        SubActionButton buttonIcon4 = itemBuilder.setContentView(itemIcon4).build();
+
+        // 3. Create the menu with the items:
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(buttonIcon1)
+                .addSubActionView(buttonIcon2)
+                .addSubActionView(buttonIcon3)
+                .addSubActionView(buttonIcon4)
+                .attachTo(actionButton)
+                .build();
+
+        // Making the icons clickable
+
 
 
     }
