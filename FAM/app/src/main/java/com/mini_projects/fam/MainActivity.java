e359d80904bb64459da5e53b09525baceb861623
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-04 20:04:12 +0300

    OnClick: Linking Icon Buttons on FAM to Activities

--- FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
+++ FAM/app/src/main/java/com/mini_projects/fam/MainActivity.java
@@ -3,24 +3,14 @@ package com.mini_projects.fam;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    //tags for the buttonIcons
-    public static final String TAG_itemIcon1 = "icon1";
-    public static final String TAG_itemIcon2 = "icon2";
-    public static final String TAG_itemIcon3 = "icon3";
-    public static final String TAG_itemIcon4 = "icon4";
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,13 +49,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         SubActionButton buttonIcon3 = itemBuilder.setContentView(itemIcon3).build();
         SubActionButton buttonIcon4 = itemBuilder.setContentView(itemIcon4).build();
 
-        buttonIcon1.setTag(TAG_itemIcon1);
-        buttonIcon2.setTag(TAG_itemIcon2);
-        buttonIcon3.setTag(TAG_itemIcon3);
-        buttonIcon4.setTag(TAG_itemIcon4);
-
         // 3. Create the menu with the items:
-
         FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
                 .addSubActionView(buttonIcon1)
                 .addSubActionView(buttonIcon2)
@@ -73,61 +57,37 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 .addSubActionView(buttonIcon4)
                 .attachTo(actionButton)
                 .build();
+
+        buttonIcon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, ActivityOne.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, ActivityTwo.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, ActivityThree.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, ActivityFour.class);
+                startActivity(a);
+            }
+        });
+
     }
 
-
-    private void buttonIcon1Click() {
-        Intent intent1 = new Intent(this, ActivityOne.class);
-        startActivity(intent1);
-    }
-
-    private void buttonIcon2Click() {
-        Intent intent2 = new Intent(this, ActivityTwo.class);
-        startActivity(intent2);
-    }
-
-    private void buttonIcon3Click() {
-        Intent intent3 = new Intent(this, ActivityThree.class);
-        startActivity(intent3);
-    }
-
-    private void buttonIcon4Click() {
-        Intent intent4 = new Intent(this, ActivityFour.class);
-        startActivity(intent4);
-    }
-
-    public void onClick(View v) {
-        if (TAG_itemIcon1.equals(v.getTag())){
-            buttonIcon1Click();
-        }
-        if (TAG_itemIcon2.equals(v.getTag())){
-            buttonIcon2Click();
-        }
-        if (TAG_itemIcon3.equals(v.getTag())){
-            buttonIcon3Click();
-        }
-        if (TAG_itemIcon4.equals(v.getTag())){
-            buttonIcon4Click();
-        }
-
-
-//        switch (v.getTag()) {
-//            case equals():
-//                buttonIcon1Click();
-//                break;
-//
-//            case buttonIcon2:
-//                buttonIcon2Click();
-//                break;
-//
-//            case buttonIcon3:
-//                buttonIcon3Click();
-//                break;
-//
-//            case buttonIcon4:
-//                buttonIcon4Click();
-//                break;
-
-        }
-    }
+}
 
