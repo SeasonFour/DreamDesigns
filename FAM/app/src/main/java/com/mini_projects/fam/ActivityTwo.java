2cf7b9a977c531e1d97fffc52c9941ccb68888ec
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-10 14:08:44 +0300

    Floating Action Menu added to Activity One, Two, Three, Four

--- FAM/app/src/main/java/com/mini_projects/fam/ActivityTwo.java
+++ FAM/app/src/main/java/com/mini_projects/fam/ActivityTwo.java
@@ -1,8 +1,14 @@
 package com.mini_projects.fam;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 public class ActivityTwo extends Activity {
 
@@ -10,5 +16,81 @@ public class ActivityTwo extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        //---------------------------------------------------------------//
+        //Floating Action Menu//
+
+
+        // 1. Create a button to attach the menu:
+        // in Activity Context
+        ImageView imageView1 = new ImageView(this); // Create an icon
+//        imageView1.setImageResource(R.drawable.red_selected);
+
+        com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton actionButton =
+                new com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.Builder(this)
+                        .setContentView(imageView1)
+                        .build();
+
+        // 2. Creating Menu Items
+        // Icons for the 'appearing' objects - repeat many times:
+        ImageView itemIcon1 = new ImageView(this);
+        itemIcon1.setImageResource(R.drawable.arena_50);
+
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageResource(R.drawable.coins_50);
+
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageResource(R.drawable.phone_50);
+
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageResource(R.drawable.wifi_50);
+
+        //Creating Menu Items - buttons for the 4 icons
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+        SubActionButton buttonIcon1 = itemBuilder.setContentView(itemIcon1).build();
+        SubActionButton buttonIcon2 = itemBuilder.setContentView(itemIcon2).build();
+        SubActionButton buttonIcon3 = itemBuilder.setContentView(itemIcon3).build();
+        SubActionButton buttonIcon4 = itemBuilder.setContentView(itemIcon4).build();
+
+        // 3. Create the menu with the items:
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(buttonIcon1)
+                .addSubActionView(buttonIcon2)
+                .addSubActionView(buttonIcon3)
+                .addSubActionView(buttonIcon4)
+                .attachTo(actionButton)
+                .build();
+
+        buttonIcon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(ActivityTwo.this, ActivityOne.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(ActivityTwo.this, ActivityTwo.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(ActivityTwo.this, ActivityThree.class);
+                startActivity(a);
+            }
+        });
+        buttonIcon4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(ActivityTwo.this, ActivityFour.class);
+                startActivity(a);
+            }
+        });
+
+        //---------------------------------------------------------------//
     }
 }
